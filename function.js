4855f46b8532e8686b11e861945dc1a8b6be9b02
Prashii06 <prashii2806@gmail.com>
2026-05-12 11:01:41 +0530

    Added function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+function isEven(num){
+    if(num%2==0){
+        console.log(num + " is an even number.");
+    }
+    else{
+        console.log(num + " is an odd number.");
+    }
+}
+isEven(36);
+isEven(15);
+
+
+//Function Types Normal and arrow function
